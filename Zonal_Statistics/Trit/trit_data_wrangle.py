7584a92b5a58f55164405de444de1f253cd1a68b
ChrisRFarrow1 <ChrisRFarrow@protonmail.com>
2023-03-18 11:44:28 -0500

    Completed the basic calcs and output to a file.

--- Zonal_Statistics/Trit/trit_data_wrangle.py
+++ Zonal_Statistics/Trit/trit_data_wrangle.py
@@ -6,6 +6,7 @@ input_file_1 = 'Trit_2020_check090622.csv'
 input_file_2 = 'Trit_zonal_statistics.csv'
 base_output_file = 'Python_data_processing_output/trit_Conc_Comparison.csv'
 calc1_output_file = 'Python_data_processing_output/calc1_trit_Conc_Comparison.csv'
+calc2_output_file = 'Python_data_processing_output/Trit_Final_Calc_Output.csv'
 output_false_file = 'Python_data_processing_output/trit_FALSE_Conc_Values.csv'
 conc_thres = 0.01
 GT_OnePercent = 1
@@ -35,7 +36,7 @@ df_merge=df_merge[df_merge['Conc']>conc_thres]
 df_merge.to_csv(calc1_output_file)
 
 # Set the headers to be used for the percent values calculated below.
-output_headers = ['Total Row Count', 'Over 1 Percent', 'Over 2.5 Percent', 'Over 5 Percent', 'Over 10 Percent','Over 25 Percent', 'Over 50 Percent']
+headers = ['Total Row Count', 'Count Over 1 Percent', 'Count Over 2.5 Percent', 'Count Over 5 Percent', 'Count Over 10 Percent','Count Over 25 Percent', 'Count Over 50 Percent']
 
 # Calculate from the "Percent_Diff" column the number of cells over a specific percentage value.
 column = df_merge[colName] # 
@@ -48,29 +49,11 @@ Percents.append(column[column > GT_TenPercent].count()) # Over 10 % for Cell 1 i
 Percents.append(column[column > GT_TwentyFivePercent].count()) # Over 25 % for Cell 1 in df_output_headers.
 Percents.append(column[column > GT_FiftyPercent].count()) # Over 50 % for Cell 1 in df_output_headers.
 # Create the Pandas Data Frame with the output data and the correct headers.
-df_calcs = pd.DataFrame(Percents, columns = output_headers)
+df_calcs = pd.DataFrame(Percents).transpose() # = output_headers)
+df_calcs.columns = headers # Set the headers on the new Data Frame, df_calcs
 
+# Output the new Data Frame to a CSV file.
+df_calcs.to_csv(calc2_output_file)
 
-
-
-#list_thres = [5, 10, 25, 50, 100,1000, 10000]
-#col_names = [f'th{i} for i in {list_thres}']
-
-
-#col_names = ['th5','th10' .... 'th10000']
-
-# count = []
-# for i in list_thres:
-#     print(i)
-#     df_tmp = df_merge[df_merge['Conc']>i]
-#     print(df_tmp.shape)
-#     ncells = df_tmp.shape[0] #number of rows in df df_tmp
-#     count.append(ncells)
-# #
-# print(count)
-
-# dfstat = pd.DataFrame(data=count, columns=col_names)
-# dfstat.to_csv('test.csv')
-
-# th5     th10
-# 55367   55367
+# Output to the screen the final data frame
+print(df_calcs)
