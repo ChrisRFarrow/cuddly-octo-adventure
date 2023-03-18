561c3f522e03a02437170e734ce3bc72b6434ed0
ChrisRFarrow1 <ChrisRFarrow@protonmail.com>
2023-03-18 11:26:07 -0500

    Count values for various percentage values.

--- Zonal_Statistics/Trit/trit_data_wrangle.py
+++ Zonal_Statistics/Trit/trit_data_wrangle.py
@@ -1,66 +1,76 @@
 import pandas as pd
 import numpy as np
 
+# Variables Declaration Section 
 input_file_1 = 'Trit_2020_check090622.csv'
 input_file_2 = 'Trit_zonal_statistics.csv'
-output_file = 'Python_data_processing_output/trit_Conc_Comparison.csv'
+base_output_file = 'Python_data_processing_output/trit_Conc_Comparison.csv'
+calc1_output_file = 'Python_data_processing_output/calc1_trit_Conc_Comparison.csv'
 output_false_file = 'Python_data_processing_output/trit_FALSE_Conc_Values.csv'
 conc_thres = 0.01
+GT_OnePercent = 1
+GT_TwoFivePercent = 2.5
+GT_FivePercent = 5
+GT_TenPercent = 10
+GT_TwentyFivePercent = 25
+GT_FiftyPercent = 50
+colName = 'Percent_Diff'
 
-# First dataframe (cell centroid values)
+# Two Data Frame declarations from two input files. These two will be merged into a single Data Frame.
+# First Data Frame (Cell centroid values)
 df1 = pd.read_csv(input_file_1)
-#df1 = df1[df1['Conc']>10]
-# #df1.plot(y='Conc')
-
-# First dataframe (cell centroid values)
+# Second Data Frame (Zonal Statistics calculated from qGIS)
 df2 = pd.read_csv(input_file_2)
 
-# Merge two dataframes
+# Merge the two dataframes.
 df_merge = pd.merge(df1, df2, how='left', on=['I','J'])
 # Remove rows without data in the listed columns, such as _count, _sum, etc.
 df_merge = df_merge.dropna(subset=["_count","_sum","_mean","_median","_min","_max","_range"])
-df_merge.to_csv(output_file, sep=',')
-#df_merge = pd.read_csv(output_file)
+df_merge.to_csv(base_output_file, sep=',')
 
-# Check which values are not the same, write to the 'df_merge' data frame
-#df_merge['Conc_sum_Comparison_Output'] = np.where((df_merge['Conc']==df_merge['_sum']), "True", "False")
-#df_merge['Conc_mean_Comparison_Output'] = np.where((df_merge['Conc']==df_merge['_mean']), "True", "False")
-#df_merge['Conc_median_Comparison_Output'] = np.where((df_merge['Conc']==df_merge['_median']), "True", "False")
-#df_merge['Conc_min_Comparison_Output'] = np.where((df_merge['Conc']==df_merge['_min']), "True", "False")
-#df_merge['Conc_max_Comparison_Output'] = np.where((df_merge['Conc']==df_merge['_max']), "True", "False")
-# Export the merged dataframe to a CSV file format.
-#df_merge.to_csv(output_file, sep=',')
-
-#Calculate the percent difference between 'Conc' (concentration) and the 
-# calculated '_mean' value.
-df_merge['Diff'] = (abs(df_merge['Conc']-df_merge['_mean']))/((df_merge['Conc'] + df_merge['_mean'])/2)
+#Calculate the percent difference between 'Conc' (concentration) and the calculated '_mean' value.
+df_merge['Percent_Diff'] = 100*(abs(df_merge['Conc']-df_merge['_mean']))/((df_merge['Conc'] + df_merge['_mean'])/2)
 df_merge=df_merge[df_merge['Conc']>conc_thres]
+# Output the Data Frame for general use, as needed.
+df_merge.to_csv(calc1_output_file)
 
-list_thres = [5, 10, 25, 50, 100,1000, 10000]
-col_names = [f'th{i} for i in {list_thres}']
+# Set the headers to be used for the percent values calculated below.
+output_headers = ['Total Row Count', 'Over 1 Percent', 'Over 2.5 Percent', 'Over 5 Percent', 'Over 10 Percent','Over 25 Percent', 'Over 50 Percent']
+
+# Calculate from the "Percent_Diff" column the number of cells over a specific percentage value.
+column = df_merge[colName] # 
+Percents = [] # List to store each percent value calculated.
+Percents.append(len(df_merge.Percent_Diff)) # Total Row Count for Cell 0 in df_output_headers.
+Percents.append(column[column > GT_OnePercent].count()) # Over 1 % for Cell 1 in df_output_headers.
+Percents.append(column[column > GT_TwoFivePercent].count()) # Over 2.5 % for Cell 1 in df_output_headers.
+Percents.append(column[column > GT_FivePercent].count()) # Over 5 % for Cell 1 in df_output_headers.
+Percents.append(column[column > GT_TenPercent].count()) # Over 10 % for Cell 1 in df_output_headers.
+Percents.append(column[column > GT_TwentyFivePercent].count()) # Over 25 % for Cell 1 in df_output_headers.
+Percents.append(column[column > GT_FiftyPercent].count()) # Over 50 % for Cell 1 in df_output_headers.
+# Create the Pandas Data Frame with the output data and the correct headers.
+df_calcs = pd.DataFrame(Percents, columns = output_headers)
 
 
-col_names = ['th5','th10' .... 'th10000']
 
-count = []
-for i in list_thres:
-    print(i)
-    df_tmp = df_merge[df_merge['Conc']>i]
-    print(df_tmp.shape)
-    ncells = df_tmp.shape[0] #number of rows in df df_tmp
-    count.append(ncells)
-#
-print(count)
 
-dfstat = pd.DataFrame(data=count, columns=col_names)
-dfstat.to_csv('test.csv')
+#list_thres = [5, 10, 25, 50, 100,1000, 10000]
+#col_names = [f'th{i} for i in {list_thres}']
+
+
+#col_names = ['th5','th10' .... 'th10000']
+
+# count = []
+# for i in list_thres:
+#     print(i)
+#     df_tmp = df_merge[df_merge['Conc']>i]
+#     print(df_tmp.shape)
+#     ncells = df_tmp.shape[0] #number of rows in df df_tmp
+#     count.append(ncells)
+# #
+# print(count)
+
+# dfstat = pd.DataFrame(data=count, columns=col_names)
+# dfstat.to_csv('test.csv')
 
 # th5     th10
 # 55367   55367
-
-    
-    
-
-# Create a new data frame for the rows that generated a "FALSE" in the 'Comparison_Output' column
-#df_FALSE = df_merge[df_merge.Comparison_Output == "False"] 
-#df_FALSE.to_csv(output_false_file, sep=',')
